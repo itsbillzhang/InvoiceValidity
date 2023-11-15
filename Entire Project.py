c670ba441cfd0ab88205b6f1c07b8ad2321d30f8
Bill Zhang <40607234+itsbillzhang@users.noreply.github.com>
2023-11-15 11:10:02 -0500

    formatting

--- Entire Project.py
+++ Entire Project.py
@@ -1,25 +1,4 @@
-# Databricks notebook source
-# MAGIC %md
-# MAGIC
-# MAGIC This notebook shows you how to create and query a table or DataFrame loaded from data stored in Azure Blob storage.
 
-# COMMAND ----------
-
-# MAGIC %md
-# MAGIC
-# MAGIC ### Step 1: Set the data location and type
-# MAGIC
-# MAGIC There are two ways to access Azure Blob storage: account keys and shared access signatures (SAS).
-# MAGIC
-# MAGIC To get started, we need to set the location and type of the file.
-
-# COMMAND ----------
-
-storage_account_name = "invoices1"
-storage_container_name = "invoicecontainer"
-storage_account_access_key = "YOUR_ACCESS_KEY"
-
-# COMMAND ----------
 
 dbutils.fs.mount(
   source = "wasbs://invoicecontainer@invoices1.blob.core.windows.net/",
